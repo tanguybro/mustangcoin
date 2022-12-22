be502e2cabe895992870b87920ca0375f2e4cf09
tanguybro <tanguy.brouassin@gmail.com>
2022-12-22 10:58:30 +0100

    add test

--- test/MustangCoin.test.ts
+++ test/MustangCoin.test.ts
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import { utils } from 'ethers';
 import { ethers } from 'hardhat';
 
 describe('Test MustangCoin', async () => {
@@ -12,28 +13,22 @@ describe('Test MustangCoin', async () => {
         contract = await Contract.deploy();
     });
 
-    it('Should have 1000000 tokens of total supply', async () => {
-        const supply = await contract.SUPPLY();
-        expect(supply).to.equal(1000000 * 10 ** 18);
-    });
-
     it('Only minters can mint', async () => {
         await expect(contract.connect(otherAccount).mint(1)).to.be.reverted;
     });
 
     it('Minter can mint', async () => {
-        await contract.connect(deployer).addMinter(deployer);
+        await contract.connect(deployer).addMinter(deployer.address);
         const balanceBeforeMint = await contract.balanceOf(deployer.address);
         expect(balanceBeforeMint).to.equal(0);
 
-        await contract.connect(deployer).mint(1);
+        await contract.connect(deployer).mint(utils.parseEther('1.0'));
 
         const balanceAfterMint = await contract.balanceOf(deployer.address);
-        expect(balanceAfterMint).to.equal(1);
+        expect(balanceAfterMint).to.equal(utils.parseEther('1.0'));
     });
 
     it("Can't mint more than supply", async () => {
-        await contract.connect(deployer).addMinter(deployer);
-        await expect(contract.connect(deployer).mint(1000001)).to.be.reverted;
+        await expect(contract.connect(deployer).mint(utils.parseEther('1000000.0'))).to.be.reverted;
     });
 });
